17e2dc3a4e671ebefb7ebb6371e2506ed5d2a5bf
Oguzhan <126396389+oguzhanfak@users.noreply.github.com>
2024-03-01 02:04:56 +0300

    Update currency.js

--- currency.js
+++ currency.js
@@ -1,6 +1,7 @@
 class Currency{
     constructor(){
-        this.url="https://api.freecurrencyapi.com/v1/latest?apikey=fca_live_JGGaCEvYHHHBjcurmcR62oD63SN5u7kMDPdnaekw&base_currency=";
+        const api_key=process.env.API_KEY
+        this.url="api_key&base_currency=";
         
     }
     async exchange(amount,firstCurrency,secondCurrency){
